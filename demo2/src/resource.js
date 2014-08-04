0613f9b782f01bc753726d51144ca75d272f15d2
Ben Lee <lzlzj0407@gmail.com>
2014-08-04 09:12:47 +0800

    demo2 1.0版本

---
+++ demo2/src/resource.js
@@ -0,0 +1,19 @@
+var res = {
+    pause_png: "res/老版资源/pause.png",
+    futura_ttf: "res/老版资源/Futura.TTF",
+    item_5pts: "res/老版资源/5pts.png",
+    item_10pts: "res/老版资源/10pts.png",
+    item_50pts: "res/老版资源/50pts.png",
+    item_100pts: "res/老版资源/100pts.png",
+    item_bomb: "res/老版资源/bomb.png",
+    item_freeze: "res/老版资源/freeze.png",
+    item_multiply2: "res/老版资源/x2.png",
+    item_multiply05: "res/老版资源/x0.5.png",
+    item_multiply1: "res/老版资源/x1.png",
+    shoot_hole: "res/老版资源/弹孔.png"
+};
+
+var g_resources = [];
+for(var i in res){
+    g_resources.push(res[i]);
+}
