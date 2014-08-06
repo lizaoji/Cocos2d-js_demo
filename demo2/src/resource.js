41cc97cdf6208a9db28b8d9973f9ab56152e3a1e
Ben Lee <lzlzj0407@gmail.com>
2014-08-07 01:08:26 +0800

    demo2 v1.1

--- demo2/src/resource.js
+++ demo2/src/resource.js
@@ -1,16 +1,11 @@
 var res = {
-    pause_png: "res/老版资源/pause.png",
-    futura_ttf: "res/老版资源/Futura.TTF",
-    item_5pts: "res/老版资源/5pts.png",
-    item_10pts: "res/老版资源/10pts.png",
-    item_50pts: "res/老版资源/50pts.png",
-    item_100pts: "res/老版资源/100pts.png",
-    item_bomb: "res/老版资源/bomb.png",
-    item_freeze: "res/老版资源/freeze.png",
-    item_multiply2: "res/老版资源/x2.png",
-    item_multiply05: "res/老版资源/x0.5.png",
-    item_multiply1: "res/老版资源/x1.png",
-    shoot_hole: "res/老版资源/弹孔.png"
+    virus_png: "res/virus.png",
+    bomb_png: "res/bomb.png",
+    freeze_png: "res/freeze.png",
+    slow_png: "res/slow.png",
+    addHp_png: "res/addhp.png",
+    clean_png: "res/clean.png",
+    shoot_hole: "res/shoot_hole.png"
 };
 
 var g_resources = [];
